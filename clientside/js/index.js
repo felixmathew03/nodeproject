b6d96d51a09e3bf35ed324d0247691aead60014b
felixmathew03 <felixmathewtt@gmail.com>
2024-09-06 22:13:16 +0530

    gg

---
+++ clientside/js/index.js
@@ -0,0 +1,63 @@
+async function getstudents() {
+    const res=await fetch("http://localhost:3000/getstudents");
+    const data=await res.json();
+    str=``;
+    data.map((dt)=>{
+        str+=`
+        <div class="content">
+            <table class="table1">
+                <tr>
+                    <th><label for="name">Name</label></th>
+                    <td>:</td>
+                    <td align="center"><input type="text" value=${dt.name} disabled="true" name="name" id="name-${dt._id}"></td>
+                </tr>
+                <tr>
+                    <th><label for="rno">Roll No.</label></th>
+                    <td>:</td>
+                    <td align="center"><input type="text" value=${dt.rollno} disabled="true" name="rno" id="rno-${dt._id}"></td>
+                </tr>
+                <tr>
+                    <th><label for="class">Class</label></th>
+                    <td>:</td>
+                    <td align="center"><input type="text" value=${dt.class} disabled="true" name="class" id="class-${dt._id}"></td>
+                </tr>
+                <tr>
+                    <th>Subjects</th>
+                    <td>:</td>
+                    <td>
+                        <table>
+                            <tr>
+                                <th><label for="maths">Mathematics</label></th>
+                                <td>:</td>
+                                <td align="center"><input disabled="true" value=${dt.physics} type="text" name="maths" id="maths-${dt._id}"></td>
+                            </tr>
+                            <tr>
+                                <th><label for="physics">Physics</label></th>
+                                <td>:</td>
+                                <td align="center"><input disabled="true" value=${dt.maths} type="text" name="physics" id="physics-${dt._id}"></td>
+                            </tr>
+                            <tr>
+                                <th><label for="chemistry">Chemistry</label></th>
+                                <td>:</td>
+                                <td align="center"><input disabled="true" type="text" value=${dt.chemistry} name="chemistry" id="chemistry-${dt._id}"></td>
+                            </tr>
+                        </table>
+                        
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="center">   
+                        <button id="edit">EDIT</button>
+                        <button id="save">SAVE</button>
+                        <button id="delete">DELETE</button>
+                    </td>
+                </tr>
+            </table>
+        </div>   
+        `
+    });
+ 
+    document.getElementById("main").innerHTML=str
+}
+
+getstudents();
---
+++ serverside/server.js
@@ -0,0 +1,69 @@
+const PORT=3000;
+const http=require("http")
+const fs=require("fs")
+const url=require("url")
+const queryString=require("querystring")
+const {MongoClient,ObjectId}=require("mongodb")
+//connect database
+const client=new MongoClient("mongodb://127.0.0.1:27017/")
+
+const app=http.createServer(async(req,res)=>{
+    //create database
+    const db=client.db("Student")
+    //create collection
+    const collection=db.collection("details")
+    const path=url.parse(req.url)
+    console.log(path);
+
+    if(path.pathname=="/"){
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.end(fs.readFileSync("../clientside/index.html"))
+    }
+    else if(path.pathname=="/js/index.js"){
+        res.writeHead(200,{"Content-Type":"text/js"});
+        res.end(fs.readFileSync("../clientside/js/index.js"))
+    }
+    else if(path.pathname=="/pages/add"){
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.end(fs.readFileSync("../clientside/pages/add.html"))
+    }
+
+
+    //fetch data from add donor
+    if(path.pathname=="/submit" && req.method=="POST"){
+        let body="";
+        req.on("data",(chunks)=>{
+            body+=chunks.toString();
+            console.log(body);
+        });
+        req.on("end",async()=>{
+            //convert query string to obj
+            const formData=queryString.parse(body);
+            //insert to colllection
+            collection.insertOne(formData).then(()=>{
+                console.log("success");
+            }).catch((error)=>{
+                console.log(error);
+            });
+        });
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.end(fs.readFileSync("../clientside/index.html"))
+    }
+
+    if(path.pathname=="/getstudents" && req.method=="GET"){
+        const data = await collection.find().toArray();
+        const jsonData=JSON.stringify(data);
+        console.log(jsonData);
+        res.writeHead(200,{"Content-Type":"text/json"});
+        res.end(jsonData)
+    }
+});
+
+client.connect().then(()=>{
+    console.log("database connected");
+    app.listen(PORT,()=>{
+        console.log(`server at http://localhost:${PORT}/`);
+    });
+}).catch((error)=>{
+    console.log(error);
+})
